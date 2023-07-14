460e20e3f064081c82a94f9b22985f01652f8b71
bela <nagy.bela.budapest@gmail.com>
2023-07-14 22:57:39 +0200

    képek

--- main.py
+++ main.py
@@ -13,22 +13,14 @@ class PizzaApp(QMainWindow):
         self.comboBox_2.currentIndexChanged.connect(self.calculate_price)
 
         # CheckBox-okhoz csatlakoztatott eseménykezelők
-        self.checkBox.stateChanged.connect(self.update_table)
-        self.checkBox_2.stateChanged.connect(self.update_table)
-        self.checkBox_3.stateChanged.connect(self.update_table)
-        self.checkBox_4.stateChanged.connect(self.update_table)
-        self.checkBox_5.stateChanged.connect(self.update_table)
-        self.checkBox_6.stateChanged.connect(self.update_table)
-        self.checkBox_7.stateChanged.connect(self.update_table)
-        self.checkBox_8.stateChanged.connect(self.update_table)
-        self.checkBox_9.stateChanged.connect(self.update_table)
-        self.checkBox_10.stateChanged.connect(self.update_table)
-        self.checkBox_11.stateChanged.connect(self.update_table)
-        self.checkBox_12.stateChanged.connect(self.update_table)
-        self.checkBox_13.stateChanged.connect(self.update_table)
-        self.checkBox_14.stateChanged.connect(self.update_table)
-        self.checkBox_15.stateChanged.connect(self.update_table)
-        self.checkBox_16.stateChanged.connect(self.update_table)
+        checkboxes = [
+            self.checkBox, self.checkBox_2, self.checkBox_3, self.checkBox_4,
+            self.checkBox_5, self.checkBox_6, self.checkBox_7, self.checkBox_8,
+            self.checkBox_9, self.checkBox_10, self.checkBox_11, self.checkBox_12,
+            self.checkBox_13, self.checkBox_14, self.checkBox_15, self.checkBox_16
+        ]
+        for checkbox in checkboxes:
+            checkbox.stateChanged.connect(self.update_table)
 
         # Táblázat inicializálása
         self.tableWidget.setColumnCount(2)
@@ -109,30 +101,12 @@ class PizzaApp(QMainWindow):
         self.total_price = price
         for row in range(self.tableWidget.rowCount()):
             price_item = self.tableWidget.item(row, 1)
-            item_price = int(price_item.text())
-            self.total_price += item_price
+            if price_item:
+                item_price = int(price_item.text())
+                self.total_price += item_price
 
         self.label_total_price.setText(f"Összesen: {self.total_price} Ft")
 
-    def clear_selections(self):
-        self.comboBox.setCurrentIndex(0)
-        self.comboBox_2.setCurrentIndex(0)
-        self.checkBox.setChecked(False)
-        self.checkBox_2.setChecked(False)
-        self.checkBox_3.setChecked(False)
-        self.checkBox_4.setChecked(False)
-        self.checkBox_5.setChecked(False)
-        self.checkBox_6.setChecked(False)
-        self.checkBox_7.setChecked(False)
-        self.checkBox_8.setChecked(False)
-        self.checkBox_9.setChecked(False)
-        self.checkBox_10.setChecked(False)
-        self.checkBox_11.setChecked(False)
-        self.checkBox_12.setChecked(False)
-        self.checkBox_13.setChecked(False)
-        self.checkBox_14.setChecked(False)
-        self.checkBox_15.setChecked(False)
-        self.checkBox_16.setChecked(False)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
